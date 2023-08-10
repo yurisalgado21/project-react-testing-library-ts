05387a227dd499c7aedc0fbfdde716734f839f65
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-08-10 15:25:41 -0300

    Concertando o lint

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,20 +1,20 @@
-import { getByText, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('1. Teste o componente <App.tsx />', () => {
   test('O primeiro link deve ter o texto Home.', () => {
-    const { user } = renderWithRouter(<App />);
+    renderWithRouter(<App />);
     expect(screen.getByRole('link', {
       name: /home/i,
     })).toBeInTheDocument();
   });
   test('O segundo link deve ter o texto About.', () => {
-    const { user } = renderWithRouter(<App />);
+    renderWithRouter(<App />);
     expect(screen.getByRole('link', { name: /about/i })).toBeInTheDocument();
   });
   test('O terceiro link deve ter o texto Favorite Pokémon.', () => {
-    const { user } = renderWithRouter(<App />);
+    renderWithRouter(<App />);
     expect(screen.getByRole('link', { name: /Favorite Pokémon/i })).toBeInTheDocument();
   });
   test('Teste se a aplicação é redirecionada para a página inicial, na URL /, ao clicar no link Home da barra de navegação.', async () => {
