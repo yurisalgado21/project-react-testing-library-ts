1ab7152d5ce98ed015b40005e06b5a67b4d51da4
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-08-10 18:01:32 -0300

    Exercício 4 feito

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,21 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('4. Teste o componente <NotFound.tsx />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<App />, { route: '/xablau' });
+    const headingNotFound = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+    expect(headingNotFound).toBeInTheDocument();
+  });
+  test("Teste se a página mostra a imagem com o texto alternativo Clefairy pushing buttons randomly with text I have no idea what i 'm doing", () => {
+    renderWithRouter(<App />, { route: '/xablau' });
+    const imgNotFound = screen.getByRole('img', {
+      name: /clefairy pushing buttons randomly with text i have no idea what i'm doing/i,
+    });
+    const srcImageNotFound = imgNotFound.getAttribute('alt');
+    expect(srcImageNotFound).toBe("Clefairy pushing buttons randomly with text I have no idea what i'm doing");
+  });
+});
